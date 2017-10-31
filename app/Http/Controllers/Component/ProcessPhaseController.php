3265a9a7f02e36de8622680d0360c45c1b3bb33c
Daniel BC <danibeca@okazo.co>
2017-10-31 18:57:48 -0500

    new phase process service.

--- app/Http/Controllers/Component/ProcessPhaseController.php
+++ app/Http/Controllers/Component/ProcessPhaseController.php
@@ -9,10 +9,13 @@ use App\Models\ContinuousIntegration\ProcessPhase;
 use Illuminate\Http\Request;
 
 
-
 class ProcessPhaseController extends ApiController
 {
 
+    public function index($componentId)
+    {
+        return $this->respondStandard(ProcessPhase::where('component_owner_id', $componentId)->get());
+    }
 
     public function store(Request $request, $id)
     {
