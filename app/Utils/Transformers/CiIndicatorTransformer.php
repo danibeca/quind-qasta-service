2ddeecd0a0fc68921021470cdb64228f740b73a8
Daniel BC <danibeca@okazo.co>
2017-10-30 20:49:54 -0500

    Adding Ci indicators services.

--- app/Utils/Transformers/CiIndicatorTransformer.php
+++ app/Utils/Transformers/CiIndicatorTransformer.php
@@ -10,13 +10,21 @@ class   CiIndicatorTransformer extends Transformer
 
     public function transform($indicator)
     {
-        $ciIndicator = CiIndicator::find($indicator['id']);
 
-        $value = isset($indicator['value']) ? $indicator['value'] : 0;
-        return [
-            'date'  => Carbon::parse($indicator['created_at'])->format('d-m-Y'),
-            'value' => round($value, 2),
-            'name'  => $ciIndicator->name
-        ];
+
+        if ($indicator)
+        {
+            $ciIndicator = CiIndicator::find($indicator['id']);
+            $value = isset($indicator['value']) ? $indicator['value'] : 0;
+
+            return [
+                'date'  => Carbon::parse($indicator['created_at'])->format('d-m-Y'),
+                'value' => round($value, 2),
+                'name'  => $ciIndicator->name
+            ];
+        } else
+        {
+            return [];
+        }
     }
 }
