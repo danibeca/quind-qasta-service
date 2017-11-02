c41bbd992b416d96c593b864e2fc05dff1214fb5
Daniel BC <danibeca@okazo.co>
2017-11-02 10:39:03 -0500

    Tracking problem

--- app/Http/Controllers/Component/ComponentQualityAttributeValueController.php
+++ app/Http/Controllers/Component/ComponentQualityAttributeValueController.php
@@ -7,13 +7,14 @@ use App\Models\Component\ComponentAttributeIssueSerie;
 use App\Http\Controllers\ApiController;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Log;
 
 
 class ComponentQualityAttributeValueController extends ApiController
 {
     public function store($componentId)
     {
-
+        Log::info(json_encode(Input::all()));
         ComponentAttributeIssueSerie::where('attribute_id', (Input::get('attribute_id')))->where('component_id', $componentId)->where('created_at','>',Carbon::now()->format('Y-m-d'))->delete();
         $value = new ComponentAttributeIssueSerie(Input::all());
         $value->component_id = $componentId;
